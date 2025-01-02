23c7ef46e40b8dc902a98f7c077ba2658c29362a
Collins Mutai <collinsfrontend@gmail.com>
2025-01-03 00:32:30 +0300

    ui

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroSliderComponent } from '../hero-slider/hero-slider.component';
+import { AboutComponent } from "../about/about.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroSliderComponent],
+  imports: [HeroSliderComponent, AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,34 +1,45 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css',
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
   menuOpen = false;
   menuClosing = false;
+  isFixed: boolean = false;
+  isTopNavbarFixed: boolean = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollPosition = window.scrollY;
+    this.isFixed = scrollPosition > 50;
+    this.isTopNavbarFixed = scrollPosition > 100;
+  }
 
   toggleMenu() {
     if (this.menuOpen) {
-      // If the menu is already open, trigger the close animation
       this.menuClosing = true;
       setTimeout(() => {
-        this.menuOpen = !this.menuOpen; // Actually hide the menu after the animation
-        this.menuClosing = false; // Reset closing state
-      }, 500); // Match the duration of the animation (500ms)
+        this.menuOpen = !this.menuOpen;
+        this.menuClosing = false;
+      }, 500);
     } else {
-      // If the menu is closed, open it with the slide-in animation
       this.menuOpen = !this.menuOpen;
     }
   }
 
   navigateTo(page: string) {
     console.log(`Navigating to ${page}`);
-    // Uncomment the following line if using Angular Router
-    // this.router.navigate([page]);
+    this.toggleMenu();
+
+    const element = document.getElementById(page)!;
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
   }
 }
